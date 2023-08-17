f005fcef02d6ceca7144289aed6cecf8e4c918a5
workerone1 <25370927-workerone1@users.noreply.replit.com>
2023-08-17 09:05:25 +0000

    created schema for mongo db

--- myApp.js
+++ myApp.js
@@ -1,26 +1,36 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true });
-  let Person;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+})
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-    done(null /*, data*/);
+  done(null /*, data*/);
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-    done(null /*, data*/);
+  done(null /*, data*/);
 };
 
 const findPeopleByName = (personName, done) => {
-    done(null /*, data*/);
+  done(null /*, data*/);
 };
 
 const findOneByFood = (food, done) => {
-    done(null /*, data*/);
+  done(null /*, data*/);
 };
 
 const findPersonById = (personId, done) => {
-    done(null /*, data*/);
+  done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
@@ -36,7 +46,7 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-    done(null /*, data*/);
+  done(null /*, data*/);
 };
 
 const removeManyPeople = (done) => {
@@ -51,20 +61,20 @@ const queryChain = (done) => {
   done(null /*, data*/);
 };
 
-  /** **Well Done !!**
-  /* You completed these challenges, let's go celebrate !
-   */
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
 
-  //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
-  exports.PersonModel = Person;
-  exports.createAndSavePerson = createAndSavePerson;
-  exports.findPeopleByName = findPeopleByName;
-  exports.findOneByFood = findOneByFood;
-  exports.findPersonById = findPersonById;
-  exports.findEditThenSave = findEditThenSave;
-  exports.findAndUpdate = findAndUpdate;
-  exports.createManyPeople = createManyPeople;
-  exports.removeById = removeById;
-  exports.removeManyPeople = removeManyPeople;
-  exports.queryChain = queryChain;
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
