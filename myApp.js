42a787e74322f49c4c574b3a378ee3779b12bcea
workerone1 <25370927-workerone1@users.noreply.replit.com>
2023-08-17 08:16:58 +0000

    task connect to mongo db

--- myApp.js
+++ myApp.js
@@ -1,26 +1,26 @@
 require('dotenv').config();
-
-
-let Person;
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true });
+  let Person;
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
@@ -36,7 +36,7 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const removeManyPeople = (done) => {
@@ -51,20 +51,20 @@ const queryChain = (done) => {
   done(null /*, data*/);
 };
 
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
+  /** **Well Done !!**
+  /* You completed these challenges, let's go celebrate !
+   */
 
-//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+  //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
-exports.PersonModel = Person;
-exports.createAndSavePerson = createAndSavePerson;
-exports.findPeopleByName = findPeopleByName;
-exports.findOneByFood = findOneByFood;
-exports.findPersonById = findPersonById;
-exports.findEditThenSave = findEditThenSave;
-exports.findAndUpdate = findAndUpdate;
-exports.createManyPeople = createManyPeople;
-exports.removeById = removeById;
-exports.removeManyPeople = removeManyPeople;
-exports.queryChain = queryChain;
+  exports.PersonModel = Person;
+  exports.createAndSavePerson = createAndSavePerson;
+  exports.findPeopleByName = findPeopleByName;
+  exports.findOneByFood = findOneByFood;
+  exports.findPersonById = findPersonById;
+  exports.findEditThenSave = findEditThenSave;
+  exports.findAndUpdate = findAndUpdate;
+  exports.createManyPeople = createManyPeople;
+  exports.removeById = removeById;
+  exports.removeManyPeople = removeManyPeople;
+  exports.queryChain = queryChain;
