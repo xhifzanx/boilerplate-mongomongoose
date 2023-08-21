3346b511845878424806e7bee080e6a7049e1268
xhifzanx <88442980+xhifzanx@users.noreply.github.com>
2023-08-21 08:02:26 +0000

    used remove function to remove one or more people with same name

--- myApp.js
+++ myApp.js
@@ -95,8 +95,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, function(err, data) {
+    if (err) console.error(err)
+    done(null, data)
+  })
 };
 
 const queryChain = (done) => {
