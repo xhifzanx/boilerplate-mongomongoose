63a2daa2046c6978d62f5c881cea0dd4ca2d5f21
xhifzanx <88442980+xhifzanx@users.noreply.github.com>
2023-08-21 08:25:50 +0000

    used chain query

--- myApp.js
+++ myApp.js
@@ -103,8 +103,10 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find().sort().limit(2).select({ age: 0 }).exec(function(err, data) {
+    if (err) console.error(err)
+    done(null, data)
+  })
 };
 
 /** **Well Done !!**
